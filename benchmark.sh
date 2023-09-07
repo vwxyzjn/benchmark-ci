3a72f0280ee8902204620772a210aa7307f681f2
Costa Huang <costa.huang@outlook.com>
2023-09-07 20:08:25 +0000

    push changes

--- benchmark.sh
+++ benchmark.sh
@@ -1,1 +1,10 @@
-poetry run python test_ci/ppo.py --total-timesteps 10000
+python benchmark/benchmark.py \
+    --command "poetry run python test_ci/ppo.py --total-timesteps 10000 --track" \
+    --num-seeds 5 \
+    --start-seed 1 \
+    --workers 10 \
+    --slurm-nodes 1 \
+    --slurm-gpus-per-task 1 \
+    --slurm-ntasks 1 \
+    --slurm-total-cpus 12 \
+    --slurm-template-path benchmark/trl.slurm_template

77aa52a4f44afccec07934a46e476f90ab68c209
Costa Huang <costa.huang@outlook.com>
2023-09-08 20:28:44 +0000

    quick change

--- benchmark/post.sh
+++ benchmark/post.sh
@@ -1,10 +1,12 @@
 # job_ids="430592,430593"
 regex_ids=$(echo $job_ids | tr ',' '|')
+
+echo got $job_ids
 echo $regex_ids
 
 while squeue -j $job_ids | grep -qE "$regex_ids"; do
-    echo "waiting for jobs to finish"
     sleep 10
+    echo "waiting for jobs to finish"
 done
 
 python benchmark/post_github_comment.py

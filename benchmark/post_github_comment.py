656ca08063dd11e3c352cc4caf21202fff968a78
Costa Huang <costa.huang@outlook.com>
2023-09-08 20:13:22 +0000

    test

--- benchmark/post_github_comment.py
+++ benchmark/post_github_comment.py
@@ -12,7 +12,7 @@ body = status_message
 repo = github_context["repository"]
 owner, repo = repo.split("/")
 # Create a GitHub API instance
-api = GhApi(owner=owner, repo=repo, token=os.environ['GITHUB_PAT_SERVICE_WORKER_TOKEN'])
+api = GhApi(owner=owner, repo=repo, token=github_context["token"])
 
 # Create a comment on the issue
 api.issues.create_comment(issue_number=github_context["event"]["issue"]["number"], body=body)

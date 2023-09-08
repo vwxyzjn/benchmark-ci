ab679760e97e4eeea2af4f64615809d40da7019f
Costa Huang <costa.huang@outlook.com>
2023-09-08 14:12:22 +0000

    update

--- test.py
+++ test.py
@@ -5,13 +5,13 @@ import json
 
 github_context = json.loads(os.environ['GITHUB_CONTEXT'])
 
-
-
 status_message = f": Here are the benchmark results"
 body = status_message 
 
+repo = github_context["repository"]
+owner, repo = repo.split("/")
 # Create a GitHub API instance
-api = GhApi()
+api = GhApi(owner=owner, repo=repo, token=github_context["token"])
 
 # Create a comment on the issue
-api.issues.create_comment(issue_number=github_context["github.event.issue.number"], body=body)
+api.issues.create_comment(issue_number=github_context["event"]["issue"]["number"], body=body)

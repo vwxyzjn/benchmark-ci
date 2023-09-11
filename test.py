902f91bf2d6e20ddad4ab37179df7d80920da4a2
Costa Huang <costa.huang@outlook.com>
2023-09-11 13:35:23 -0400

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ status_message = f": Here are the benchmark results"
 body = status_message 
 
 repo = github_context["repository"]
-owner, repo = repo.split("/")
+owner, repo = repo.split("/") 
 # Create a GitHub API instance
 api = GhApi(owner=owner, repo=repo, token=github_context["token"])
 
